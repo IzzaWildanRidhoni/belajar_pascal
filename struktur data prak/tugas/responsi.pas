c22f21127e5a82837c66d8faa3fac01456c666b6
izza wildan ridhoni <izzawildan88@gmail.com>
2021-06-14 13:16:25 +0700

    update responsi dari ady

--- struktur data prak/tugas/responsi.pas
+++ struktur data prak/tugas/responsi.pas
@@ -4,43 +4,43 @@
 // Nama : Ady Putra Ramadhan
 // Nim  : 201057008
 // ---------------------------------
-
+ 
 program ANTRIAN_BIOSKOP;
 uses crt;
-
+ 
 const max = 20;
 type rec_antrian = record
     no_antri:array[1..max] of byte; //array didalam record
     front,rear:0..max;
     end;
-
+ 
 type rec_pelayanan = record 
     kode_pelayanan,nama,film:string;
     bayar,kursi:longint;
     end;
     larikbioskop = array[1..max] of rec_pelayanan; // array record
-
+ 
 type rec_film = record
      nama,jam_tayang:String;
      harga:LongInt;
      end;
      larikFilm = array[1..max] of rec_film;
-
-var	antrian:rec_antrian;
+ 
+var antrian:rec_antrian;
     bioskop:larikbioskop;
     film:larikFilm;
     pil,i,jumplay,nom_antri:byte;
-
+ 
 function isFull(A:rec_antrian):Boolean;
 begin
   if  A.rear=max then isFull:=true else isFull:=False;
 end;
-
+ 
 function isEmpty(A:rec_antrian):Boolean;
 begin
   if  A.rear=0 then isEmpty:=true else isEmpty:=False;
 end;
-
+ 
 procedure daftarFilm(var film:larikFilm);
 begin
   clrscr;
@@ -55,7 +55,7 @@ begin
   WriteLn('-----------------------------------------------');
   
 end;
-
+ 
 procedure ambil_antrian(var A:rec_antrian);
 var kurang:Byte;
 begin
@@ -68,7 +68,7 @@ begin
   WriteLn('anda mendapatkan antrian nomor  : ',nom_antri);
   WriteLn;
   WriteLn('---------------------------------------------');
-
+ 
   kurang:=(A.rear+1)-A.front; 
   
   if kurang = 0 then WriteLn('sekarang yang akan dilayani adalah anda')
@@ -78,7 +78,7 @@ begin
   A.no_antri[A.rear]:=nom_antri;  
   WriteLn('mohon bersabar menunggu :)');
 end;
-
+ 
 procedure layanan_bioskop(var A:rec_antrian;var P:larikbioskop;var F:larikFilm);
 var kode,nm_film:String;ada:Boolean;pos,kursi:Integer;
 label ulang,ulangfilm,ulangkursi;
@@ -93,7 +93,7 @@ begin
         A.no_antri[i]:=A.no_antri[i+1];
       end;
       Dec(A.rear);
-
+ 
     ulang:
     
     // masuk ke  pelayanan tiket bioskop
@@ -107,12 +107,12 @@ begin
         end;
       end;
     Inc(jumplay);
-
+ 
     P[jumplay].kode_pelayanan:=kode; 
     Write('masukkan nama lengkap        : ');ReadLn(P[jumplay].nama);
     ulangfilm:
     Write('nama film yang akan ditonton  : ');ReadLn(nm_film);
-
+ 
     // cek apakah ada film di rec_film
     for i:=1 to 3 do
     begin
@@ -141,11 +141,121 @@ begin
         WriteLn;
         WriteLn('pelayanan  berhasil  ;) ');
     end;
-
+ 
 end;
-
-
-
+ 
+procedure cetak_antrian(var A:rec_antrian);
+begin
+    writeln('Daftar antrian saat ini di Loket Tiket Bioskop');
+    writeln('-------------------------');
+    writeln('Posisi   No Antrian:');
+    writeln('-------------------------');
+    for i:=A.front to A.rear do writeln(i:4,'     ',A.no_antri[i]:4);
+    writeln('-------------------------');
+    writeln('List yang mengantre saat ini :',A.rear,' orang');
+end;
+ 
+procedure cetak_layanan(var P:larikbioskop;F:larikfilm);
+ begin
+   writeln('DAFTAR PEMBELI YANG SUDAH MEMBELI TIKET');
+   writeln('BIOSKOP');
+   writeln('---------------------------------------');
+   writeln('No  Film      Nama   Kursi');
+   writeln('---------------------------------------');
+   for i:=1 to jumplay do
+   writeln(i:2,'  ',F[i].nama:4,'  ',P[i].nama:4,'  ',P[i].kursi:3);
+   writeln('---------------------------------------');
+end;
+ 
+procedure layanan_prioritas(var A:rec_antrian;var P:larikbioskop;var F:larikfilm);
+var kode,nm_film:string;prio,pos,kursi:integer;ada:boolean;
+label ulang,ulangfilm,ulangkursi;
+begin
+  ada:=false;
+  writeln('Selamat datang di layanan prioritas Bioskop');
+  write('masukkan nomor antrian pembeli yang diprioritaskan : ');readln(prio);
+  for i:=A.front to A.rear do
+  begin if A.no_antri[i]=prio then begin ada:=true;pos:=i;end;end;
+  if not ada then writeln('Nomor antrian',prio,' tidak ada di daftar penumpang yang sedang antri saat ini')
+  else
+  begin
+  for i:=A.front to A.rear-1 do
+      begin
+        A.no_antri[i]:=A.no_antri[i+1];
+      end;
+      Dec(A.rear);
+ 
+    ulang:
+  Write('masukkan  kode pelayanan  : ');ReadLn(kode);
+    for i:=1 to jumplay do
+      begin
+        if P[i].kode_pelayanan = kode then  
+        begin
+          WriteLn('kode salah , ulangi ');
+          goto ulang; 
+        end;
+      end;
+    Inc(jumplay);
+ 
+    P[jumplay].kode_pelayanan:=kode; 
+    Write('masukkan nama lengkap        : ');ReadLn(P[jumplay].nama);
+    ulangfilm:
+    Write('nama film yang akan ditonton  : ');ReadLn(nm_film);
+    for i:=1 to 3 do
+    begin
+      if nm_film = F[i].nama then
+      begin
+        ada:=true;
+        pos:=i;
+      end;
+    end;
+    if not ada then begin WriteLn('Maaf film yang anda inputkan tidak ada , ulangi !');goto ulangfilm; end;
+    begin
+        P[jumplay].film :=nm_film;
+        WriteLn('jumlah yang harus di bayar  : Rp ',F[pos].harga);
+        WriteLn('film akan tayang jam   :  ',F[pos].jam_tayang);
+        ulangkursi:
+        Write('masukkan no kursi < no 1 - 100 >   :  ');ReadLn(kursi);
+            if kursi > 100 then begin writeln('kursi tidak tersedia , hanya tersedia no  1-100 ');goto ulangkursi; end;
+            // cek kursi
+            for i:=1 to jumplay do
+              begin
+                if kursi = P[i].kursi then begin
+                 WriteLn('Kursi sudah dipesan , orang lain');goto ulangkursi;
+                end; 
+              end;
+        P[jumplay].kursi:=kursi;
+        WriteLn;
+        WriteLn('pelayanan  berhasil  ;) ');
+    end;
+ end; 
+end;
+ 
+procedure keluar_antrian(var A:rec_antrian);
+var nom:byte;ada:boolean;pos:byte;ya:char;
+begin
+  ada:=false;
+  writeln('Keluar antrian Tiket Bioskop');
+  write('masukkan nomor antrian yang keluar :');readln(nom);
+  for i:=A.front to A.rear do
+  begin
+      if A.no_antri[i]=nom then begin ada:=true;pos:=i;end;
+  end;
+  if not ada then writeln('Nomor antrian ', nom,' tidak ada didalam Data')
+  else
+  begin
+    writeln('Nomor antrian ',nom,' ada diposisi',pos);
+    writeln('Yakin akan keluar dari antrian<y/t>?');readln(ya);
+    if (ya='Y')or(ya='y')then
+    begin
+        writeln('Terimakasih, Silahkan datang lagi');
+        for i:=pos to A.rear do
+            A.no_antri[i]:=A.no_antri[i+1];
+        dec(A.rear);
+    end else writeln('Nomor antrian ',nom,' tidak jadi keluar antrian');
+end;
+end;
+ 
 // main program
 begin
     // inisialisasi
@@ -153,7 +263,7 @@ begin
     antrian.rear:=0;
     jumplay:=0;
     nom_antri:=0;
-
+ 
     // daftar film yang disediakan 
     film[1].nama:='avenger';
     film[1].harga:=12000;
@@ -166,7 +276,7 @@ begin
     film[3].nama:='one punch man';
     film[3].harga:=30000;
     film[3].jam_tayang:='14.00 - 16.00 WIB';
-
+ 
     repeat
       clrscr;
       WriteLn('====================================');
@@ -175,17 +285,25 @@ begin
       WriteLn('1. Daftar Film Bioskop ');
       WriteLn('2. Ambil Antrian Tiket ');
       writeln('3. Layanan Tiket');
+      writeln('4. Cetak Data Antrian');
+      writeln('5. Cetak Data Layanan');
+      writeln('6. Prioritas Layanan');
+      writeln('7. Keluar Antrian');
       WriteLn('0. exit');
       Write('pilih layanan : ');ReadLn(pil);
-
+ 
       case pil of 
         1:daftarFilm(film);
         2:if isFull(antrian) then WriteLn('maaf antrian sudah full , hanya menerima  ',max,' pelayanan saja;)') else ambil_antrian(antrian); 
         3:if isEmpty(antrian) then WriteLn('sedang tidak ada antrian ') else layanan_bioskop(antrian,bioskop,film);
-        0:WriteLn('terimakasih ') else WriteLn('anda salah pilih menu ');
+        4:if IsEmpty(antrian)then writeln('Sedang tidak ada antrian')else cetak_antrian(antrian);
+        5:if jumplay=0 then writeln('Belum ada penumpang yang sdh dilayani')else cetak_layanan(bioskop,film);
+        6:if IsEmpty(antrian)then writeln('Sedang tidak ada antrian')else layanan_prioritas(antrian,bioskop,film);
+        7:if IsEmpty(antrian)then writeln('Sedang tidak ada antrian')else keluar_antrian(antrian);
+        0:WriteLn('Terimakasih Karena Telah Menggunakan Program Kami:)') else WriteLn('anda salah pilih menu ');
       end;
     ReadLn;
-
+ 
     // akan menjalankan program sampai user menginputkan pilihan menu sama dengan 0
     until pil = 0;
 end.
